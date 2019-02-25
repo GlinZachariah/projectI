4b1b818987e5e227482d25ba871c6cd23683d7a3
GlinZachariah <glincool.zac@gmail.com>
2019-02-25 20:51:50 +0530

    completed file generation

--- index.py
+++ index.py
@@ -1,20 +1,67 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import csv
+import re
 from PyPDF2 import PdfFileReader
 
+sub=["APPLIED ELECTRONICS & INSTRUMENTATION ENGINEERING","CIVIL ENGINEERING","MECHANICAL ENGINEERING","ELECTRONICS & COMMUNICATION ENGG","COMPUTER SCIENCE & ENGINEERING","ELECTRICAL AND ELECTRONICS ENGINEERING"]
+subject_split_format=r"AE[0-9]+|EE[0-9]+|CS[0-9]+|IT[0-9]+|EC[0-9]+|CE[0-9]+|ME[0-9]+|HS[0-9]+"
+reg_split_format=r"RET[0-9]+AE[0-9]+|RET[0-9]+EE[0-9]+|RET[0-9]+CS[0-9]+|RET[0-9]+IT[0-9]+|RET[0-9]+EC[0-9]+|RET[0-9]+CE[0-9]+|RET[0-9]+ME[0-9]+"
+sub1_dt={}
+sub1_list_dt={}
+
+document_name ="result_RET.csv"
 
 def text_extractor(path):
+    finalpage="";
     with open(path, 'rb') as f:
         pdf = PdfFileReader(f)
-        for pg in range(pdf.numPages):
-            page = pdf.getPage(pg)
-            print (page)
-            text = page.extractText()
-            print (text)
+        page = pdf.getPage(0)
+        text = page.extractText()
+        simpleText="[Full Time]Course CodeCourse"
+        val=text.split(sub[0]+simpleText)
+        newval=val[1].split("Register NoCourse Code (Grade)")
+        x = re.findall(subject_split_format, newval[0])
+        y = re.split(subject_split_format, newval[0])[1:]
+        # print(x)
+        # print(y)
+        if len(x) == len(y):
+            for i in range(len(x)):
+                sub1_dt[x[i]]=y[i]
+        else:
+            print("failed")
+        # print (sub1_dict)
+        m = re.findall(reg_split_format, newval[1])
+        n = re.split(reg_split_format, newval[1])[1:]
+        # print(newval[1])
+        n[0]=n[0].strip()
+        df=n[0].split(',')
+        with open(document_name, mode='w') as csv_file:
+            result_writer = csv.writer(csv_file, delimiter='^', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            result_writer.writerow(['University No. ,'+','.join(list(sub1_dt.keys()))])
+            row=m[0]
+            for i in range(len(df)):
+                for j in range(len(sub1_dt)):
+                    if list(sub1_dt.keys())[j] in df[i]:
+                        # print(list(sub1_dt.keys())[j]+ " =>"+df[i] )
+                        x= df[i][:-1].strip().split("(")[1]
+                        x=","+x
+                        # x = re.split(r',',)
+                        row+=x
+                        # print(x)
+            result_writer.writerow([row])
+        # q=re.split(, df[0])
+        # print(n[0])
+        print(df)
+        # print(q[0])
+        print(m)
+        # print(list(sub1_dt.keys())[1])
+
+
+			
 
 
 if __name__ == '__main__':
     path = 'result_RET.pdf'
-    page = text_extractor(path)
-    print (page)
+
+    text_extractor(path)
---
+++ main.py
@@ -0,0 +1,102 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import csv
+from PyPDF2 import PdfFileReader
+import re
+import os
+
+course=["APPLIED ELECTRONICS & INSTRUMENTATION ENGINEERING","ELECTRONICS & COMMUNICATION ENGG","COMPUTER SCIENCE & ENGINEERING","ELECTRICAL AND ELECTRONICS ENGINEERING","INFORMATION TECHNOLOGY","MECHANICAL ENGINEERING","CIVIL ENGINEERING"]
+subject_split_format=r"AE[0-9]+|EE[0-9]+|CS[0-9]+|IT[0-9]+|EC[0-9]+|CE[0-9]+|ME[0-9]+|HS[0-9]+"
+reg_split_format=r"RET[0-9]+AE[0-9]+|RET[0-9]+EE[0-9]+|RET[0-9]+CS[0-9]+|RET[0-9]+IT[0-9]+|RET[0-9]+EC[0-9]+|RET[0-9]+CE[0-9]+|RET[0-9]+ME[0-9]+"
+filename=["AE","EC","CS","EE","IT","ME","CE"]
+
+def text_extractor(path):
+    finalpage="";
+    with open(path, 'rb') as f:
+        pdf = PdfFileReader(f)
+        for pg in range(pdf.numPages):
+            page = pdf.getPage(pg)
+            text = page.extractText()
+            finalpage+=text
+    return finalpage
+			
+def split_courses(pages,dat):
+    simpleText="[Full Time]Course CodeCourse"
+    #fix subject order if necessary
+    pre= pages.split(course[0]+simpleText)
+    temp=pre[1].split(course[1]+simpleText)
+    applied = temp[0]
+    temp=temp[1].split(course[2]+simpleText)
+    ec=temp[0]
+    temp=temp[1].split(course[3]+simpleText)
+    cse=temp[0]
+    temp=temp[1].split(course[4]+simpleText)
+    eee=temp[0]
+    temp=temp[1].split(course[5]+simpleText)
+    it=temp[0]
+    temp=temp[1].split(course[6]+simpleText)
+    mech=temp[0]
+    temp=temp[1].split(dat)
+    civil=temp[0]
+    ls= [applied,ec,cse,eee,it,mech,civil]
+    count=0
+    for newl in ls:
+        split_subjects(newl,count)
+        count+=1
+
+def split_subjects(newl,cnt):
+    sub1_dt={}
+    newval=newl.split("Register NoCourse Code (Grade)")
+    subject_code = re.findall(subject_split_format, newval[0])
+    subject_name = re.split(subject_split_format, newval[0])[1:]
+    if len(subject_code) == len(subject_name):
+        for i in range(len(subject_code)):
+            sub1_dt[subject_code[i]]=subject_name[i]
+    else:
+        print("Subject count error")
+        return
+    make_csv(newval,cnt,subject_code)
+
+def make_csv(newval,cnt,subject_code):
+    reg_no = re.findall(reg_split_format, newval[1])
+    sub_list = re.split(reg_split_format, newval[1])[1:]
+    with open("result_"+filename[cnt]+".csv", mode='w',newline='') as csv_file:
+            result_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            # result_writer = csv.writer(csv_file, delimiter=',', escapechar="'", quoting=csv.QUOTE_NONE)
+            result_writer.writerow(['University No. ,'+','.join(subject_code)])
+            for i in range(len(reg_no)):
+                df=reg_no[i].strip().split(",")
+                cdf=sub_list[i]
+                cdf_list = cdf.split(",")
+                row=df[0]+','
+                grade=''
+                for k in subject_code:
+                    flag=0
+                    for l in cdf_list:
+                        if  l.strip()[:5]in k:
+                            flag=1
+                            grade=l.strip()[:-1].split("(")[1]
+                            grade=grade+','
+                            row+=grade
+                    if flag==0:
+                        grade=','
+                        row+=grade
+                result_writer.writerow([row])
+    with open("result_"+filename[cnt]+".csv", 'r') as f, open("result_RET_"+filename[cnt]+".csv", 'w') as fo:
+        for line in f:
+            fo.write(line.replace('"', '').replace("'", ""))
+    os.remove("result_"+filename[cnt]+".csv")
+        
+    
+
+if __name__ == '__main__':
+    # path = input('Enter filename : (result_RET.pdf)')
+    path="result_RET.pdf"
+    # order= input('Enter order of courses')
+    # input the last end date to split the document
+    dat="08/10/2018"
+    if path == "":
+        path="result_RET.pdf"
+    page = text_extractor(path)
+    split_courses(page,dat)
+    # print (page)
